ba2a1dba61d12a70b4f27cd47cb3889106755e58
maabhavani <abhishekthakur646@gmail.com>
2023-03-03 12:14:42 +0530

    dombytag

---
+++ dombytagname.js
@@ -0,0 +1,46 @@
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent="hello";
+// //items[1].style.fontWeight="bold";
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor="#f4f4f4";
+// }
+// items[2].style.backgroundColor="green";
+
+
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+
+// }
+// for(var i=0;i<items.length;i++){
+//     items[i].style.color='#EF3024';
+// }
+
+
+
+
+
+
+
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent="hello";
+//li[1].style.fontWeight="bold";
+for(var i=0;i<li.length;i++){
+li[i].style.backgroundColor="#f4f4f4";
+}
+li[2].style.backgroundColor="green";
+
+
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
+
+}
+for(var i=0;i<li.length;i++){
+li[i].style.color='#EF3024';
+}
