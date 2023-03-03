1d217bedfb5b5980b7b24f1cf1221d8580402ff1
maabhavani <abhishekthakur646@gmail.com>
2023-03-03 13:23:38 +0530

    querry

---
+++ dombyquerryselector.js
@@ -0,0 +1,34 @@
+// var header=document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+
+
+// var input=document.querySelector('input');
+// input.value='hello';
+
+// var submit=document.querySelector('input[type="submit"]');
+// submit.value='ok';
+
+
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+// seconditem.style.backgroundColor='green';
+
+// var thirditem=document.querySelector('.list-group-item:nth-child(3');
+// thirditem.style.visibility='hidden';
+
+
+
+
+var title=document.querySelectorAll('.title');
+console.log(title);
+title[0].textContent='hello';
+var seconditem=document.querySelectorAll('li');
+seconditem[1].style.color='green';
+ 
+var odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
